e4d86e4dde620cff6b886d0aa7f48a77dc21c95c
Nadupoy <gnsampao@gmail.com>
2024-04-01 12:49:17 +0300

    Trying really hard to configure the client and server 😰. First commit.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,42 @@
 import ReactDOM from "react-dom/client";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [output, getOutput] = useState("");
+
+  // useEffect(() => {
+  //   fetch("http://localhost:8000")
+  //     .then((res) => res.json())
+  //     .then((data) => createCipher(data.message));
+  // }, []);
+
   return (
     <section className="app-component">
       <h2>ENCRYPTOR/DECRYPTOR:</h2>
 
-      <form id="app">
+      <form id="app" method="post" onSubmit=""> {/*<form> sends data to the server
+      for encryption/decryption
+      */}
         <label htmlFor="message" form="app">
           Input message here:
         </label>
         <br />
-        <input type="text" required={true} maxLength="50" id="message" />
+        <input type="text" required={true} maxLength="50" id="message" />{" "}
+        {/* 
+        value is sent (i.e.'post') to the server for encryption or decryption
+        */}
         <br />
-        <input type="button" value="ENCRYPT/DECRYPT" />
+        <input type="submit" value="ENCRYPT/DECRYPT" />
         <br />
         <input type="reset" value="RESET" id="reset-button" />
       </form>
 
       <p className="output">
-        Output: <span></span>
+        Output: <span>{output}</span>{" "}
+        {/*the encrypted/decrypted message is displayed
+        (i.e.'fetch') here from the server
+         */}
       </p>
     </section>
   );
@@ -27,15 +44,15 @@ function App() {
 
 function Attribution() {
   return (
-      <footer>
-        <p>
-          Built by{" "}
-          <a href="https://github.com/nadupoy" target="_blank">
-            Nadupoy
-          </a>
-          .
-        </p>
-      </footer>
+    <footer>
+      <p>
+        Built by{" "}
+        <a href="https://github.com/nadupoy" target="_blank">
+          Nadupoy
+        </a>
+        .
+      </p>
+    </footer>
   );
 }
 
--- server/app.js
+++ server/app.js
@@ -1,11 +1,27 @@
+// Import Express and CORS:
 const express = require("express");
+const cors = require("cors");
+// Create an Express application:
 const app = express();
-const port = 3000;
+const port = 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+/*
+Since this is a SPA, add a middleware function (i.e. 'use()') with no mount
+path. The function is executed everytime app.js recieves a request.
+*/
+// app.use((req, res) => {
+//   console.log("Hello from the server!");
+// });
+app.use(express.json());
+app.get('/result', (req, res) => {
+  res.json({message: "Hello from server!"});
 });
 
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
